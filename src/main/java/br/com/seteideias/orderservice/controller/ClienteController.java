98467124927bdbeb5882ef12a21362c4a5d16cf7
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2022-04-23 23:32:45 -0300

    ajustes no controller

--- src/main/java/br/com/seteideias/orderservice/controller/ClienteController.java
+++ src/main/java/br/com/seteideias/orderservice/controller/ClienteController.java
@@ -4,11 +4,7 @@ import br.com.seteideias.orderservice.domain.Cliente;
 import br.com.seteideias.orderservice.repository.ClienteRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -16,32 +12,38 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
-
     @Autowired
     private ClienteRepository repository;
 
     @GetMapping
-    public ResponseEntity<Cliente> list() {
+    public Iterable<Cliente> list() {
 
-        Iterable<Cliente> list = repository.findAll();
+        Iterable<Cliente> all = repository.findAll();
+        all.forEach(cliente -> new Cliente());
 
-        return ResponseEntity.ok(list.iterator().next());
+        return all;
     }
 
     @GetMapping(path = "/{id}")
     public Cliente view(@PathVariable String id) {
 
-        Optional<Cliente> byId = repository.findById(id);
+        Optional<Cliente> cliente = repository.findById(id);
 
-        if (byId.isPresent()) {
-            log.info("byId " + byId);
-            return byId.get();
 
-        } else {
-            log.info("byId " + byId);
-            return null;
+        if (cliente.isPresent()) {
+            return cliente.get();
         }
 
+        Cliente cliente1 = new Cliente();
+        cliente1.setId("cliente.get().getId()");
+        cliente1.setNomeTeste("cliente.get().getNomeTeste()");
 
+        return cliente1;
+    }
+
+    @PostMapping("/post/")
+    public void postarCliente(@RequestBody Cliente cliente) {
+        log.info("cliente " + cliente.toString());
+        repository.save(cliente);
     }
 }
--- src/main/java/br/com/seteideias/orderservice/domain/Cliente.java
+++ src/main/java/br/com/seteideias/orderservice/domain/Cliente.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "teste")
+@Document(collection = "cliente")
 public class Cliente {
 
     @Id
